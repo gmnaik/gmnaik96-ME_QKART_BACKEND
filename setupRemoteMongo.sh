ab4fbcb02fc2ae146fb3886676a60fd136244444
gmnaik <gmnaik96@gmail.com>
2023-03-22 17:31:26 +0530

    Update setupRemoteMongo.sh
    Updated url in mongoremote.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://gmnaik96:Gmnaik445@qkart-node.asd5yn0.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://gmnaik96:Gmnaik445@qkart-node.asd5yn0.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
